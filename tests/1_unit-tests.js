e017fc8e980ef606588fd848353f062d26690699
DarryDarry <da.ershova@mail.ru>
2023-08-03 07:29:48 +0300

    5 testing values of array

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -107,17 +107,14 @@ suite("Unit Tests", function () {
   const backendLanguages = ["php", "python", "javascript", "ruby", "asp"];
   suite("Arrays", function () {
     // #11
-    test("#isArray, #isNotArray", function () {
-      assert.fail(
-        "isThisAnArray?".split(""),
-        "String.prototype.split() returns an array",
-      );
-      assert.fail([1, 2, 3].indexOf(2), "indexOf returns a number");
+    test('#isArray, #isNotArray', function () {
+      assert.isArray('isThisAnArray?'.split(''), 'String.prototype.split() returns an array');
+      assert.isNotArray([1, 2, 3].indexOf(2), 'indexOf returns a number');
     });
     // #12
-    test("Array #include, #notInclude", function () {
-      assert.fail(winterMonths, "jul", "It's summer in july...");
-      assert.fail(backendLanguages, "javascript", "JS is a backend language");
+    test('Array #include, #notInclude', function () {
+      assert.notInclude(winterMonths, 'jul', "It's summer in july...");
+      assert.include(backendLanguages, 'javascript', 'JS is a backend language');
     });
   });
 
